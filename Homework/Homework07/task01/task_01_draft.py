a90f8ac6e44747a0029829f3db9714cf564b92ea
Irina Filippova <irina.filippova.mail@yandex.ru>
2023-03-20 14:01:01 +0300

    Homework 07

--- Homework/Homework07/task01/task_01_draft.py
+++ Homework/Homework07/task01/task_01_draft.py
@@ -8,12 +8,35 @@
 К ним прибавляется желаемое конечное имя, если оно передано.
  Далее счётчик файлов и расширение. """
 
-import os
-from pathlib import Path
+# import os
+# from random import randint
+#
+# def group_renaming(dn: str, cn: int):
+#     file_num = [randint(1, 10) for _ in range(cn)]
+#     file_num01 = ''.join(map(str, file_num))
+#     name_file = ''
+#     name_file += dn + file_num01
+#     print(name_file)
+#
+#     for dir_path, dir_name, file_name in os.walk(os.getcwd()):
+#         print(f'{dir_path = }\n{dir_name = }\n{file_name = }\n')
+#         for i in file_name:
+#             split_tup = os.path.splitext(i)
+#             print(f'расширение и имя файла {split_tup}')
+#             file_extension = split_tup[1]
+#             print(file_extension)
+#             if '.txt' in file_extension:
+#                 name_file += file_extension
+#                 print(name_file)
+#                 for i in file_name:
+#                     os.rename(i, name_file)
+#
+#
+#
+# desired_name = input('Введите желаемое имя: ')
+# count_num = int(input('Количество цифр в порядковом номере:'))
+#
+# group_renaming(desired_name, count_num)
 
-def group_renaming(dn: str,):
-
-    os.rename('new_os_dir', dn)
 
 
-desired_name = input('Введите желаемое имя: ')
