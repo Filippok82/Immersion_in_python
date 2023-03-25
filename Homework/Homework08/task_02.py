da79e8a5ca44a903bb322e2f1a403d92d66b9897
Irina Filippova <irina.filippova.mail@yandex.ru>
2023-03-25 19:12:49 +0300

    Homework 09 correction

--- Homework/Homework08/task_02.py
+++ Homework/Homework08/task_02.py
@@ -1,3 +1,5 @@
+"""Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами разных форматов."""
+
 from PythonGB.Seminars.seminar08.task_01 import convert
 from PythonGB.Seminars.seminar08.task_02 import name_id_access_level
 from task_01 import directory_traversal
